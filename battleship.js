bad4dfd2d9770826775ddc587116ac05691735e4
Charles Dowell <iggydowell@gmail.com>
2016-09-08 09:56:38 -0700

    edits
    added win condition, css, js

--- battleship.js
+++ battleship.js
@@ -1,4 +1,29 @@
 // sets grid rows and columns and the size of each square
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+//btn.onclick = function() {
+//    modal.style.display = "block";
+//}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+//window.onclick = function(event) {
+//    if (event.target == modal) {
+//        modal.style.display = "none";
+//    }
+//}
+
+
 var rows = 10;
 var cols = 10;
 var squareSize = 50;
@@ -64,8 +89,46 @@ var gameBoard = [
 				[1,0,0,0,0,0,0,0,0,0]
 				]
 
+				var fireInput;
+				var inputColumn;
+				var inputRow;
+				var target;
+				var firstInput;
+				var secondInput;
+				var convertedFirstInput;
+				var fireVal;
+				var inputCount = 0;
+				var winCondition = 0;
+
 function fireTorpedo() {
 
 	// Your game logic will go here!
+//modal.style.display = "block";
+inputCount++;
+document.getElementById("tries").textContent = "You have won in " + inputCount + " turns! Now restarting...";
+	fireVal = document.getElementById("blah").value;
+	firstInput = fireVal.substring(0, 1);
+	convertedFirstInput = letterConversion[firstInput];
+	secondInput = fireVal.substring(1, 3);
 
+	console.log("The first input value is " + convertedFirstInput);
+	console.log("The second input value is " + secondInput);
+	if(gameBoard[convertedFirstInput][secondInput] == 1)
+	{
+		console.log("Hit");
+		document.getElementById("s" + convertedFirstInput + (secondInput - 1)).style.backgroundColor = "red";
+		winCondition++;
+		if(winCondition == 17) {
+			modal.style.display = "block";
+			window.setTimeout(reloadPage,5000);
+			function reloadPage() {
+				location.reload();
+			}
+		}
+	}
+	else
+	{
+		document.getElementById("s" + convertedFirstInput + (secondInput - 1)).style.backgroundColor = "grey";
+		console.log("Miss");
+	}
 }
